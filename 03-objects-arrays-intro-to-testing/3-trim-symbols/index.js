740457d421dad64648e05f63df1ceb9a270ef3ef
Galina Matashnyuk <galchk@mail.ru>
2021-08-16 15:53:50 +0300

    implementation function trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,10 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let counter = 0;
+  return [...string].reduce(
+    (previousValue, item, index, array) => {
+      counter = (!index || item === array[index - 1]) ? counter + 1 : 1;
+      return counter <= size ? previousValue + item : previousValue;
+    }, '');
 }
