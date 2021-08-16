31b5849aa743bfcde93e1bc44cd5054bfa0dd66e
Galina Matashnyuk <galchk@mail.ru>
2021-08-16 12:09:26 +0300

    implementation function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,11 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const fields = path.split('.');
+  return function (Obj) {
+    const findField = (field, i) => (field === undefined) ? field : field[i];
+    return fields.reduce(findField, Obj);
+  };
 }
